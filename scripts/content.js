24fdb42e518e755aa93fec480a9f7e6d33320b61
Jack Chan <jackchw.uk@gmail.com>
2025-01-20 12:09:39 +0000

    optimize with blob

--- scripts/content.js
+++ scripts/content.js
@@ -1,12 +1,30 @@
 let currentSrc = "";
 
-document.addEventListener("mouseover", (event) => {
+const fetchImage = async (url) => {
+  let originalImageURL = new URL(url);
+  if (originalImageURL.host === "pbs.twimg.com") {
+    originalImageURL = new URL(
+      originalImageURL.origin +
+        originalImageURL.pathname +
+        "?format=jpg&name=large"
+    );
+  }
+
+  const response = await fetch(originalImageURL);
+  return response.blob();
+};
+
+document.addEventListener("mouseover", async (event) => {
   let target = event.target.closest("img");
   if (target && currentSrc !== target.currentSrc) {
     currentSrc = target.currentSrc;
+
+    const blob = await fetchImage(currentSrc);
+    const blobURL = URL.createObjectURL(blob);
+
     chrome.runtime.sendMessage({
       type: "updateImgUrl",
-      value: currentSrc,
+      value: blobURL,
     });
   }
 });
--- scripts/extension-page.js
+++ scripts/extension-page.js
@@ -1,9 +1,5 @@
 chrome.runtime.onMessage.addListener((msg) => {
   if (msg.type !== "updateImgUrl" || !msg.value) return;
-  let url = new URL(msg.value);
-  if (url.host === "pbs.twimg.com") {
-    url = new URL(url.origin + url.pathname + "?format=jpg&name=large");
-  }
 
-  document.getElementById("image").src = url;
+  document.getElementById("image").src = msg.value;
 });
