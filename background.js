8ef9d8097cb51e9936eb822cba680cb22fce5083
Jack Chan <jackchw.uk@gmail.com>
2025-01-24 19:26:34 +0000

    remove action badge text

--- background.js
+++ background.js
@@ -1,25 +1,5 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.setBadgeText({
-    text: "0",
-  });
-});
-
-let tabs = [];
-
-chrome.action.onClicked.addListener(async (tab) => {
-  // Set the action badge to the next state
-  await chrome.action.setBadgeText({
-    text: (tabs.length + 1).toString(),
-  });
-  const extensionTab = await chrome.windows.create({
+chrome.action.onClicked.addListener((tab) => {
+  chrome.windows.create({
     url: chrome.runtime.getURL("index.html"),
-  });
-  tabs.push(extensionTab.id);
-});
-
-chrome.tabs.onRemoved.addListener((tabId) => {
-  tabs = tabs.filter((id) => id !== tabId);
-  chrome.action.setBadgeText({
-    text: tabs.length.toString(),
   });
 });
